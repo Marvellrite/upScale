55836a39bbe5139648b8457707c449e12a678c2e
Marvel <melodyozuru087@gmail.com>
2024-02-09 20:14:53 +0100

    Adjust the active days selection javascript code

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,14 +1,24 @@
-const days = Array.from(document.querySelectorAll("div.daysList>.days"));
-const btnToggle = document.querySelectorAll(".toggleDateDisplay");
-const todayO = document.querySelector(".todayO");
-const dayList = document.querySelector("div.dayList");
+const days = Array.from(document.querySelectorAll("div.daysList>div.days"));
+const btnToggle = document.querySelectorAll("button.toggleDateDisplay");
+const todayHolder = document.querySelector("div.today");
+const today = document.querySelector("p.today>span");
+const dayList = document.querySelector("div.daysList");
 let date = new Date();
 
-days[date.getDay].classList.add("presentDay");
+console.log(days);
+console.log(date.getDay());
+console.log(days[date.getDay()]);
+
+days[date.getDay()].classList.add("presentDay");
 
 
-for(let i=0; btnToggle<i; i++){
+for(let i=0; i<btnToggle.length; i++){
     btnToggle[i].addEventListener("click", ()=>{ 
+        console.log("first")
+        todayHolder.classList.toggle("d-none");
+        dayList.classList.toggle("d-none");
     })
 
 }
+
+today.innerHTML = date.getDay == 0 || date.getDay == 6? "Closed today": "Open today";
