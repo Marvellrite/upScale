9400a9c9595328b40d5a6327afd37958c2ec1ecd
Marvel <melodyozuru087@gmail.com>
2024-02-19 18:43:53 +0100

    Start adjusting the javascript functionality of the services slide bar

--- public/js/general.js
+++ public/js/general.js
@@ -1,39 +1,39 @@
 const logoNav = document.querySelector("div.logoNav");
 
 
-window.addEventListener("scroll", ()=>{
-    const scrollLength = 40;
-    const scrolled = window.scrollY;
-    if(scrolled>=scrollLength && logoNav.classList.contains("logoNavNoScroll")){
-        return logoNav.classList.remove("logoNavNoScroll")
-    }
-    else if(scrolled<scrollLength && !logoNav.classList.contains("logoNavNoScroll")){
-        return logoNav.classList.add("logoNavNoScroll")
-    }
-        return;
+// window.addEventListener("scroll", ()=>{
+//     const scrollLength = 40;
+//     const scrolled = window.scrollY;
+//     if(scrolled>=scrollLength && logoNav.classList.contains("logoNavNoScroll")){
+//         return logoNav.classList.remove("logoNavNoScroll")
+//     }
+//     else if(scrolled<scrollLength && !logoNav.classList.contains("logoNavNoScroll")){
+//         return logoNav.classList.add("logoNavNoScroll")
+//     }
+//         return;
 
 
-})
+// })
 
 
 /** For the navbar */
 
-{
-    const navBarHider = document.querySelector(".hideNavbar");
-    const navConatiner = document.querySelector(".navContainer");
-    const bodyOverlay = document.querySelector(".bodyOverlay");
-    navBarHider.addEventListener("click", ()=>{
-        navConatiner.classList.toggle("moveLeft");
-        bodyOverlay.classList.toggle("bodyOverlayAction");
-        bodyOverlay.classList.toggle("d-none");
+// {
+//     const navBarHider = document.querySelector(".hideNavbar");
+//     const navConatiner = document.querySelector(".navContainer");
+//     const bodyOverlay = document.querySelector(".bodyOverlay");
+//     navBarHider.addEventListener("click", ()=>{
+//         navConatiner.classList.toggle("moveLeft");
+//         bodyOverlay.classList.toggle("bodyOverlayAction");
+//         bodyOverlay.classList.toggle("d-none");
 
-    })
+//     })
 
-    const showMenuIcon = document.querySelector(".showMenu");
-    showMenuIcon.addEventListener("click", ()=>{
-        navConatiner.classList.toggle("moveLeft");
-        bodyOverlay.classList.toggle("bodyOverlayAction");
-        bodyOverlay.classList.toggle("d-none");
-    })
+//     const showMenuIcon = document.querySelector(".showMenu");
+//     showMenuIcon.addEventListener("click", ()=>{
+//         navConatiner.classList.toggle("moveLeft");
+//         bodyOverlay.classList.toggle("bodyOverlayAction");
+//         bodyOverlay.classList.toggle("d-none");
+//     })
 
-}
+// }
