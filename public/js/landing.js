3d8c8dd9bef1e83a8b128ae14c3db0fae7f0182f
Marvel <melodyozuru087@gmail.com>
2024-02-28 22:03:13 +0100

    Add effects to the site

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,38 +1,38 @@
-const slideRadios = document.querySelectorAll(".input-radio");
-let sliderNo = 1;
-let lastTwo;
+// const slideRadios = document.querySelectorAll(".input-radio");
+// let sliderNo = 1;
+// let lastTwo;
 
-setInterval(() => {
-    if(sliderNo==slideRadios.length){
-        sliderNo=0;
+// setInterval(() => {
+//     if(sliderNo==slideRadios.length){
+//         sliderNo=0;
 
-    }
+//     }
 
-    document.querySelector(".input-radio"+sliderNo).checked = true;
-    document.querySelector(".label"+sliderNo).classList.add("selected");
-    document.querySelector(`.testimony${sliderNo}`).classList.add("show-testimony");
-    // document.querySelector(`.testimony${sliderNo}`).classList.remove("d-none");
+//     document.querySelector(".input-radio"+sliderNo).checked = true;
+//     document.querySelector(".label"+sliderNo).classList.add("selected");
+//     document.querySelector(`.testimony${sliderNo}`).classList.add("show-testimony");
+//     // document.querySelector(`.testimony${sliderNo}`).classList.remove("d-none");
 
-    document.querySelector(`.label${(sliderNo==0?slideRadios.length:sliderNo)-1}`).classList.remove("selected");
-    document.querySelector(`.testimony${(sliderNo==0?slideRadios.length:sliderNo)-1}`).classList.add("hide-left-testimony");
+//     document.querySelector(`.label${(sliderNo==0?slideRadios.length:sliderNo)-1}`).classList.remove("selected");
+//     document.querySelector(`.testimony${(sliderNo==0?slideRadios.length:sliderNo)-1}`).classList.add("hide-left-testimony");
     
 
-    if(sliderNo-2==-1){
-        lastTwo = 2;
-    }
-    else if(sliderNo-2==-2){
-        lastTwo = 1;
-    }
-    else{
-        lastTwo = sliderNo-2;
-    }
+//     if(sliderNo-2==-1){
+//         lastTwo = 2;
+//     }
+//     else if(sliderNo-2==-2){
+//         lastTwo = 1;
+//     }
+//     else{
+//         lastTwo = sliderNo-2;
+//     }
 
-    document.querySelector(`.testimony${lastTwo}`).classList.remove("hide-left-testimony", "show-testimony");
-    // setTimeout(() => {
-    //     document.querySelector(`.testimony${(sliderNo==0?slideRadios.length:sliderNo)-1}`).classList.add("d-none");
-    // }, 400);
-    sliderNo++;
-}, 5000);
+//     document.querySelector(`.testimony${lastTwo}`).classList.remove("hide-left-testimony", "show-testimony");
+//     // setTimeout(() => {
+//     //     document.querySelector(`.testimony${(sliderNo==0?slideRadios.length:sliderNo)-1}`).classList.add("d-none");
+//     // }, 400);
+//     sliderNo++;
+// }, 5000);
 
 // const servicesHolder = document.querySelector("div.servicesHolder");
 // const servicesHolderPar = document.querySelector("div.serviceHolderPar");
@@ -73,6 +73,30 @@ setInterval(() => {
 //     // })
 // }
 
-const observerMoveRight = new IntersectionObserver((entries)=>{
-    
+
+const observerMove = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry.target);
+        if (entry.isIntersecting) {
+            if (entry.target.classList.contains("textToMoveRight")) {
+                observerMove.unobserve(entry.target);       
+                return entry.target.classList.add("textMovingRight");
+            }
+
+            {
+                observerMove.unobserve(entry.target);       
+                return entry.target.classList.add("textMovingLeft");
+            }
+        }
+
+
+    }
+    )
+}, {
+    threshold: 0.6
 });
+
+
+Array.from(document.querySelectorAll(".textToMoveLeft, .textToMoveRight")).forEach((element)=>{
+    observerMove.observe(element);
+})
